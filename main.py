1bc6f0de12313b0d4da6debaba8dccd51f884fcd
Calvin Matson-Roberts <calvinmatsonroberts@outlook.com>
2024-01-09 23:47:43 -0700

    added breaks

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 known_users = ["Calvin", "McCall", "Boga", "Runey"]
 
 while True:
-    print("Hi, my name is Security Operating Ultra Program, but you can call me S.O.U.P. ")
+    print("HI!! My name is Security Onions Ultra Potatoes! You can call me S.O.U.P. ")
 
     name = input("What is your name?: ").strip().capitalize()
     if name in known_users:
@@ -12,10 +12,15 @@ while True:
             known_users.remove(name)
 
         elif remove == "n":
-            print("Great! Have a great day!")
+            print("WONDERFUL NEWS!!! Have a great day!")
+            break
     else:
         print("Hmm. I don't think I've met you yet, {}.".format(name))
         add_user = input("Would you like to be added to the system? (y/n)?: ").lower()
         if add_user == "y":
             known_users.append(name)
-            print(known_users)
+            print("Hooray! THANK YOU FOR JOINING THE S.O.U.P. SYSTEM!")
+            print("Have a nice day!")
+            break
+        elif add_user == "n":
+            print("WHAT?? PLEASE RECONSIDER!!")
